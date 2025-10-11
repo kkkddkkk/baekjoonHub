3c8d542d56afc6243005fa17c5aaf8653bf3480d
kkkddkkk <kindokyeong@gmail.com>
2025-10-11 11:30:31 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.03 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -1,13 +1,10 @@
 class Solution {
     public int solution(int[] numbers, int n) {
         int answer = 0;
-        while(true){
             for(int i:numbers){
                 answer+=i;
                 if(answer>n) break;
             }
-            break;
-        }
         return answer;
     }
 }
