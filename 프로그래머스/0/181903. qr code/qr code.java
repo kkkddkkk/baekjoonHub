4fd83a44b5d034b3973b5ed8435520316617d6bc
kkkddkkk <kindokyeong@gmail.com>
2025-10-14 22:05:51 +0900

    [level 0] Title: qr code, Time: 14.00 ms, Memory: 92.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(int q, int r, String code) {
+        String answer = "";
+        char[] codeChar = code.toCharArray();
+        for(int i=0;i<code.length();i++){
+            if(i%q==r){
+                answer+=codeChar[i];
+            }
+        }
+        return answer;
+    }
+}
